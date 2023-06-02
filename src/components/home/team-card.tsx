c2198732569a14b7151c22a0d27d8a7596a5b74d
Rediet <se.rediet.mengiste@gmail.com>
2023-06-02 11:05:49 +0300

    feat: board members data added with dymanic content and image

--- src/components/home/team-card.tsx
+++ src/components/home/team-card.tsx
@@ -1,25 +1,35 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import News from "../../../public/static/images/news.png";
 
-const TeamCard = () => {
+
+
+
+interface TeamCardProps {
+  name: string;
+  role: string;
+  detail: string;
+  image:StaticImageData;
+}
+const TeamCard = ({name,role,detail,image}:TeamCardProps) => {
   return (
     <div className="flex flex-col md:flex-row rounded-md shadow-md p-4 w-full sm:w-1/3 md:w-[600px] gap-4 flex-shrink-0">
       <div className="">
         <Image
-          src={News}
+        width={300}
+          src={image}
           alt="news"
           className="rounded-lg w-full md:w-[300px]"
         />
       </div>
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-between w-[500px]">
         <div className="text-left font-poppins-regular">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+         {detail}
         </div>
-        <div className="flex justify-between items-end">
+        <div className="flex justify-between w-full items-end">
           <div className="flex flex-col items-start">
-            <div className="font-poppins-medium text-md">Kifle Yirga</div>
+            <div className="font-poppins-medium text-md">{name}</div>
             <div className="font-poppins-regular text-gray-500 text-sm">
-              President
+             {role}
             </div>
           </div>
           <div className="text-gray-400 text-sm">Read More</div>
--- src/components/home/team-section.tsx
+++ src/components/home/team-section.tsx
@@ -3,6 +3,14 @@ import TeamCard from "./team-card";
 import { useRef } from "react";
 import { getLocaleContent } from "../../utils/localeUtil";
 import { useRouter } from "next/router";
+import kifle from "../../../public/static/images/kifle.png";
+import membere from "../../../public/static/images/menbere.jpg";
+import yonas from "../../../public/static/images/yonas.jpg";
+import asrat from "../../../public/static/images/asrat.jpg";
+
+
+
+
 
 const TeamSection = () => {
   const ref = useRef<HTMLInputElement>(null);
@@ -15,6 +23,36 @@ const TeamSection = () => {
   };
  const router = useRouter();
   const localized = getLocaleContent(router.locale as string);
+const boardMembers =[
+  {
+    id:1,
+    name:localized.board_member1,
+    role:localized.role1,
+    detail:localized.detail1,
+    image:kifle
+  },
+   {
+    id:2,
+    name:localized.board_member2,
+    role:localized.role2,
+    detail:localized.detail2,
+    image:membere
+  },
+    {
+    id:3,
+    name:localized.board_member3,
+    role:localized.role3,
+    detail:localized.detail3,
+    image:yonas
+  },  {
+    id:4,
+    name:localized.board_member4,
+    role:localized.role4,
+    detail:localized.detail4,
+    image:asrat
+  },
+
+]
 
   return (
     <div className="text-center py-10 md:py-32">
@@ -27,9 +65,11 @@ const TeamSection = () => {
         className="flex overflow-x-auto p-4 gap-8 w-full md:max-w-7xl mx-auto scrollbar-hide mb-4"
         ref={ref}
       >
-        <TeamCard />
-        <TeamCard />
-        <TeamCard />
+        {
+          boardMembers.map(member=> < TeamCard key={member.id} name={ member["name"]} role={member["role"]} detail={member["detail"]} image ={member["image"]}/>)
+        }
+     
+       
       </div>
       <div className="flex justify-end max-w-7xl mx-auto">
         <div onClick={() => scroll(-200)} className="cursor-pointer">
