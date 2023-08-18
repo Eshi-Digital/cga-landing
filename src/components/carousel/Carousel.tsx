20a24be63f5321ca6681969f86d07419ff9bde9c
AbenezerAtnafu <se.abenezer.atnafu@gmail.com>
2023-08-18 11:15:41 +0300

    carousel added

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,53 @@
+import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
+import React from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
+import CarouselControls from "./CarouselControls";
+import Dots from "./Dots";
+import Autoplay from "embla-carousel-autoplay";
+
+type Props = PropsWithChildren & EmblaOptionsType;
+
+const Carousel = ({ children, ...options }: Props) => {
+  // 1. useEmblaCarousel returns a emblaRef and we must attach the ref to a container.
+  // EmblaCarousel will use that ref as basis for swipe and other functionality.
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
+      ...options,
+      loop: true,
+    },
+    [
+      Autoplay({
+        delay: 4000,
+      }),
+    ]
+  );
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const length = React.Children.count(children);
+  const canScrollPrev = !!emblaApi?.canScrollPrev();
+  const canScrollNext = !!emblaApi?.canScrollNext();
+
+  useEffect(() => {
+    function selectHandler() {
+      // selectedScrollSnap gives us the current selected index.
+      const index = emblaApi?.selectedScrollSnap();
+      setSelectedIndex(index || 0);
+    }
+
+    emblaApi?.on("select", selectHandler);
+    // cleanup
+    return () => {
+      emblaApi?.off("select", selectHandler);
+    };
+  }, [emblaApi]);
+
+  return (
+    <>
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">{children}</div>
+      </div>
+      <Dots itemsLength={length} selectedIndex={selectedIndex} />
+    </>
+  );
+};
+export default Carousel;
--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -1,32 +1,87 @@
 import { useRouter } from "next/router";
 import { getLocaleContent } from "../../utils/localeUtil";
+import Carousel from "../carousel/Carousel";
+import Image from "next/image";
+
 const HeroSection = () => {
   const router = useRouter();
   const localized = getLocaleContent(router.locale as string);
 
+  const images = [
+    {
+      src: "/static/images/hero.png",
+      hero_title: "Slide 1",
+      hero_subtitle: "Slide 1",
+    },
+    {
+      src: "/static/images/hero.png",
+      hero_title: "Slide 2",
+      hero_subtitle: "Slide 2",
+    },
+    {
+      src: "/static/images/hero.png",
+      hero_title: "Slide 3",
+      hero_subtitle: "Slide 3",
+    },
+  ];
+
   return (
-    <div className="bg-hero h-[90vh] bg-no-repeat bg-cover flex items-center justify-center py-28">
-      <div className="flex flex-col gap-5 justify-center items-center">
-        <div className="text-center text-white text-4xl font-PoiretOne-Regular">
-        {localized.hero_title}
-        </div>
-        <div className="text-center text-white font-PoiretOne-Regular text-4xl">
-         {localized.hero_title2}
-        </div>
-        <div className="text-center text-white font-poppins-light text-md">
-         {localized.hero_subtitle}
-        </div>
-        <div className="flex-col md:flex-row flex gap-8 py-6">
+    // <div className=" h-[90vh] bg-no-repeat bg-cover flex items-center justify-center py-28">
+    <div className="h-[90vh] bg-no-repeat w-full bg-cover ">
+      <Carousel>
+        {images.map((image, index) => (
           <div
-            className="bg-primary rounded-full px-12 py-3 text-white cursor-pointer"
-            onClick={() => {
-              router.push("/about");
-            }}
+            className="relative h-[90vh] flex-[0_0_100%] flex flex-row justify-center items-end"
+            key={index}
           >
-            {localized.learn_more}
+            <Image
+              src={image.src}
+              fill
+              className="object-cover -z-10"
+              alt="alt"
+            />
+            <div className="flex flex-col gap-5 justify-center items-center">
+              <div className="text-center text-white font-PoiretOne-Regular text-4xl">
+                {image.hero_title}
+              </div>
+              <div className="text-center text-white font-poppins-light text-md">
+                {image.hero_subtitle}
+              </div>
+              <div className="flex-col md:flex-row flex gap-8 py-6">
+                <div
+                  className="bg-primary rounded-full px-12 py-3 text-white cursor-pointer"
+                  onClick={() => {
+                    router.push("/about");
+                  }}
+                >
+                  {localized.learn_more}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ))}
+        {/* <div className="flex flex-col gap-5 justify-center items-center">
+          <div className="text-center text-white text-4xl font-PoiretOne-Regular">
+            {localized.hero_title}
+          </div>
+          <div className="text-center text-white font-PoiretOne-Regular text-4xl">
+            {localized.hero_title2}
+          </div>
+          <div className="text-center text-white font-poppins-light text-md">
+            {localized.hero_subtitle}
+          </div>
+          <div className="flex-col md:flex-row flex gap-8 py-6">
+            <div
+              className="bg-primary rounded-full px-12 py-3 text-white cursor-pointer"
+              onClick={() => {
+                router.push("/about");
+              }}
+            >
+              {localized.learn_more}
+            </div>
+          </div>
+        </div> */}
+      </Carousel>
     </div>
   );
 };
