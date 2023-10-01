8347f2b8cbb4ca0312e3867919fe25b8769b44f1
Semere Talegngeta <se.semere.talegngeta@gmail.com>
2023-10-01 17:21:21 +0300

    fix: minor css fix

--- src/components/membership/membershipType.tsx
+++ src/components/membership/membershipType.tsx
@@ -11,19 +11,13 @@ const MembershipTypeAndCriteriaSection = () => {
   const [query] = useLanguageQuery();
   return (
     <div className="py-10">
-      <div className="max-w-7xl mx-auto text-justify">
-        <h1 className="mb-10 text-2xl uppercase text-center">
+      <div className="text-gray-500 max-w-7xl mx-auto text-justify">
+        <h1 className="text-black mb-10 text-2xl uppercase text-center">
           {t("membership_criteria")}
         </h1>
-        <p className="text-md font-poppins-light text-gray-500 max-w-4xl mx-auto mb-8">
-          {t("membership_1")}
-        </p>
-        <p className="text-md font-poppins-light text-gray-500 max-w-4xl mx-auto mb-8">
-          {t("membership_2")}
-        </p>
-        <p className="text-md font-poppins-light text-gray-500 max-w-4xl mx-auto mb-8">
-          {t("membership_3")}
-        </p>
+        <p className="max-w-4xl mx-auto mb-8">{t("membership_1")}</p>
+        <p className="max-w-4xl mx-auto mb-8">{t("membership_2")}</p>
+        <p className="max-w-4xl mx-auto mb-8">{t("membership_3")}</p>
       </div>
     </div>
   );
